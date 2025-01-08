c69434bbf6ecbe38f844618ae6f78bc9426e51fb
RaresMadox <raresart92@gmail.com>
2025-01-08 22:49:26 +0200

    first commit

---
+++ src/main/java/com/example/welcomeProject/WelcomeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.welcomeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/welcomeProject/WelcomeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.welcomeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
